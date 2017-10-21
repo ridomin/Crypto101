6cf3d9b9e406f0f5ef3e470cc8db276a6c83e10b
ridomin <ridomin@live.com>
2017-10-20 23:10:14 -0700

    signature basics

---
+++ Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = "Hello Signatures";
+            var signature = Crypto101.Signer.Sign(data, "cert.pfx", "Pass@Word1");
+
+            Console.WriteLine(data);
+            Console.WriteLine(signature);
+
+            var signers = Crypto101.Signer.Verify(data, signature);
+            Console.WriteLine(signers);
+
+            var bad = Crypto101.Signer.Verify(data+data, signature);
+            Console.WriteLine(bad);
+        }
+    }
+}
---
+++ Crypto101/Signer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Crypto101
+{
+    public class Signer
+    {
+        public static string Sign(string data, string certPath, string password)
+        {
+            var signedCms = new SignedCms(new ContentInfo(Encoding.UTF8.GetBytes(data)), true);
+            signedCms.ComputeSignature(new CmsSigner(new X509Certificate2(certPath, password)));                        
+            return Convert.ToBase64String(signedCms.Encode());
+        }
+        public static string Verify(string data, string signature)
+        {
+            var signedCms = new SignedCms(new ContentInfo(Encoding.UTF8.GetBytes(data)), true);
+            signedCms.Decode(Convert.FromBase64String(signature));
+            try
+            {
+                signedCms.CheckSignature(true);
+                return $"\nSignerInfo: \n Subject: {signedCms.SignerInfos[0].Certificate.Subject}" +
+                        $"\n SerialNumber: {signedCms.SignerInfos[0].Certificate.SerialNumber}" +
+                        $"\n ThumbPrint: {signedCms.SignerInfos[0].Certificate.Thumbprint}";
+
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("EXCEPTION");
+                Console.WriteLine(ex.ToString());
+                
+                return "BAD SIGNATURE";
+            }
+        }
+    }
+}
