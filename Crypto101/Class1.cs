c6bece4e83170ea60a8e5210d1b976d44b1ba79d
ridomin <ridomin@live.com>
2017-10-20 22:31:32 -0700

    Add project files.

---
+++ Crypto101/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto101
+{
+    public class Class1
+    {
+    }
+}
